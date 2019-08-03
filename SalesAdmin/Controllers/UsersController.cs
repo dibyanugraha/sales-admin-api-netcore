1a2b5c148123ee9908c1227f01a3082e098eace5
Ariestya Dibyanugraha <ariestya.dibyanugraha@gmail.com>
2019-08-03 21:27:22 +0700

    Add user controller and initial Register with Login

--- SalesAdmin/Controllers/UsersController.cs
+++ SalesAdmin/Controllers/UsersController.cs
@@ -1,10 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using SalesAdmin.Authentication;
+using SalesAdmin.Models;
 
 namespace SalesAdmin.Controllers
 {
@@ -15,19 +21,76 @@ namespace SalesAdmin.Controllers
         private readonly UserManager<User> _userManager;
         private readonly SignInManager<User> _signInManager;
         private readonly RoleManager<Role> _roleManager;
+        private readonly IConfiguration _configuration;
 
         public UsersController(
             UserManager<User> userManager,
             SignInManager<User> signInManager,
-            RoleManager<Role> roleManager
+            RoleManager<Role> roleManager,
+            IConfiguration configuration
             )
         {
             _userManager = userManager;
             _signInManager = signInManager;
             _roleManager = roleManager;
+            _configuration = configuration;
         }
 
         [HttpPost("register")]
+        public async Task<IActionResult> Register(
+            [FromBody]RegisterRequest request)
+        {
+            var user = new User
+            {
+                UserName = request.Email,
+                Email = request.Email,
+                DisplayName = request.DisplayName
+            };
+
+            var result = await _userManager.CreateAsync(user, request.Password);
+
+            if (!result.Succeeded)
+            {
+                return BadRequest(
+                    new
+                    {
+                        Errors = result.Errors.Select(
+                            a => a.Description
+                        )
+                    });
+            }
+
+            return await LoginSuccessfulResponse(user);
+        }
+
+        private async Task<IActionResult> LoginSuccessfulResponse(User user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString())
+            };
+
+            var roles = await _userManager.GetRolesAsync(user);
+            foreach (var role in roles)
+            {
+
+            }
+            var key = _configuration.GetValue<string>("Authentication:JwtKey");
+            var token = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.UtcNow.AddMinutes(30),
+                signingCredentials:
+                    new SigningCredentials(
+                        new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key)),
+                        SecurityAlgorithms.HmacSha256)
+                    );
+            var accessToken = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return Ok(new LoginSuccessResponse
+            {
+                AccessToken = accessToken
+            });
+        }
 
     }
 }
